35952316656239990443dd2eb6124a3fea60d6d6
piagja <piagja@gmail.com>
2023-09-29 01:43:05 -0300

    fix: larger search button on mobile

--- client/components/SearchButton.tsx
+++ client/components/SearchButton.tsx
@@ -26,7 +26,7 @@ export const SearchButton = () => {
   return (
     <div
       onClick={handleData}
-      className='flex self-center cursor-pointer items-center justify-evenly bg-slate-800 mt-4 h-12 w-36 rounded-lg hover:bg-slate-600 hover:text-slate-300'
+      className='flex self-center cursor-pointer items-center justify-evenly bg-slate-800 mt-4 h-12 w-36 rounded-lg hover:bg-slate-600 hover:text-slate-300 max-md:w-[270px] max-md:justify-around max-md:h-[60px] max-md:font-bold max-md:tracking-widest max-md:text-2xl'
     >
       <p className='text-lg'>Pesquisar</p>
       <AiOutlineSearch size='2rem' />
