52b9c291fa95694ac0d9cc8a44d8d632736100d4
piagja <piagja@gmail.com>
2023-10-05 01:18:49 -0300

    feat: isolate API function

---
+++ client/src/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import { IGame } from '../context/GameContext'
+
+export async function handleApi (
+  searchTerm: string,
+  selectedOption: string,
+  setGames: (games: IGame[] | null) => void,
+  setSearchTerm: (searchTerm: string) => void
+) {
+  try {
+    const response = await axios.post(
+      process.env.NEXT_PUBLIC_ENDPOINT as string,
+      {
+        searchTerm,
+        selectedOption
+      }
+    )
+
+    const games: IGame = response.data
+
+    setGames(null)
+    setSearchTerm('')
+    setGames(games)
+
+    return games
+  } catch (error) {
+    console.error('Erro ao enviar a requisição: ', error)
+    throw error
+  }
+}
